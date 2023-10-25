cfd308348ce59884ad2835b356ac4e6021abd5ab
anurag8546 <2000520100019@ietlucknow.ac.in>
2023-10-26 01:59:30 +0530

    finalized project

--- src/App.js
+++ src/App.js
@@ -6,24 +6,6 @@ import 'font-awesome/css/font-awesome.min.css';
 function App() {
   return (
     <div className="main_div">
-      {/*   
-      <div>
-       
-    
-          <div  className='div1'>
-          <div className='mplace'>Marketplace</div>
-            <div className = 'div2' style={{ color: '#24A148', fontSize: 56, fontFamily: 'Helvetica Neue', fontWeight: '400', lineHeight: 5 }}>Email Templates</div>
-          </div>
-          <div style={{ width: 1034, height: 0, left: 0, top: 100 }}>
-            <div style={{ left: 0, top: 0, position: 'absolute', color: '#161616', fontSize: 56, fontFamily: 'Helvetica Neue', fontWeight: '400', lineHeight: 5, wordWrap: 'break-word' }}>by the community, for the community.</div>
-            <div style={{ alignSelf: 'stretch', color: '#525252', fontSize: 20, fontFamily: 'Helvetica Neue', fontWeight: '400', lineHeight: 28, wordWrap: 'break-word' }}>100s of free templates to help you craft the perfect marketing journey.</div>
-        
-
-          </div>
-
-
-        
-      </div> */}
       <div className="D_1">
         <div className='mplace'> Marketplace</div>
         <div className='etemp' >Email Templates</div>
@@ -64,10 +46,10 @@ function App() {
             <div className="sort-dropdown">
               <div className="dropdown">
 
-                <div style={{ width: 150, height: 36, paddingLeft: 16, paddingRight: 16, paddingTop: 12, paddingBottom: 12, background: 'white', borderRadius: 40, /*border: '1px #C6C6C6 solid', */justifyContent: 'flex-start', alignItems: 'center', gap: 2, display: 'inline-flex' }}>
-                  <div style={{ paddingLeft: 4, paddingRight: 4, justifyContent: 'flex-start', alignItems: 'center', gap: 2, display: 'flex' }}>
-                    <div style={{ color: '#161616', fontSize: 16, fontFamily: 'Helvetica Neue', fontWeight: '500', lineHeight: 24, wordWrap: 'break-word' }}>Sort by:</div>
-                    <button className="dropdown-button">Recent</button>
+               
+                    <div className="xyz2">Sort by:</div>
+                    <div className="rec"><button className="dropdown-button">Recent</button></div>
+                    
 
                     <div className="dropdown-content">
                       <a href="#">Popular</a>
@@ -75,12 +57,8 @@ function App() {
                       <a href="#">Categories(Descending)</a>
                     </div>
                   </div>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-                    <path d="M7.25518 9.51143L9.41352 11.6698C9.73852 11.9948 10.2635 11.9948 10.5885 11.6698L12.7468 9.51143C13.2718 8.98643 12.8968 8.08643 12.1552 8.08643H7.83852C7.09685 8.08643 6.73018 8.98643 7.25518 9.51143Z" fill="#161616" />
-                  </svg>
                 </div>
-              </div>
-            </div>
+             
           </div>
           <div className="rgtbtm">
             <div className="bottom-content">
